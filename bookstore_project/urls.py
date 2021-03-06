870d416017a4b4e6019e715cfb6779bb21951c20
Shampad Sharkar <sompodsign@gmail.com>
2021-03-06 20:00:57 +0600

    Add Signup page view and test cases.

--- bookstore_project/urls.py
+++ bookstore_project/urls.py
@@ -3,6 +3,13 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    # django admin
     path('admin/', admin.site.urls),
+    # User management
+    path('accounts/', include('django.contrib.auth.urls')), # new
+    # local apps
     path('', include('pages.urls')),
+    path('accounts/', include('users.urls')),
+
+
 ]
